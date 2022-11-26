398d2c69d65aa6cad9eb42bef5d2fa383df96b49
Silvia <silviassouza@hotmail.com>
2022-11-26 14:49:47 -0300

    meu primerio projeto java

--- meu-primeiro-projeto-java/src/App.java
+++ meu-primeiro-projeto-java/src/App.java
@@ -1,5 +1,5 @@
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Boas vindas turma da Dio!");
+        System.out.println("Seja bem vinda Silvia! Aceita um café?");
     }
 }
