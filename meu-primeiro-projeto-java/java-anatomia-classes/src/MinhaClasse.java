53492e6cf35fa6675fc0c8ea11623924150d49d0
Silvia <silviassouza@hotmail.com>
2022-11-29 15:50:09 -0300

    aprendendo sobre classes

--- meu-primeiro-projeto-java/java-anatomia-classes/src/MinhaClasse.java
+++ meu-primeiro-projeto-java/java-anatomia-classes/src/MinhaClasse.java
@@ -5,5 +5,13 @@ public class MinhaClasse {
 
     System.out.print ("Olá turma, sejam bem vindos");
 
+    int ano = 2021;
+    ano = 2022;
+    ano = 2023;
+    double PI = 3.14;
+    int ESTADOS_BRASILEIRO = 27;
+    int ANO_2000 = 2000;
+    final String BR = "Brasil";
+
   }
 }
